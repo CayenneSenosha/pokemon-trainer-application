2c51841836d5dd4156f3cfd6ca6acaf04a996ae0
Cain Senosha <csenosha@jhb.dvt.co.za>
2021-10-14 14:21:04 +0200

    PTA-3 Completed the search functionality

--- pokemonTrainerApp/src/app/features/homepage/homepage.component.ts
+++ pokemonTrainerApp/src/app/features/homepage/homepage.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { ApiServiceService } from 'src/app/service/api-service.service';
 
 @Component({
   selector: 'app-homepage',
@@ -7,9 +9,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomepageComponent implements OnInit {
 
-  constructor() { }
+  searchForm: any ;
+  isLoading: boolean = false;
+  error: any = "";
+  data: any  ;
+  constructor(private formBuilder: FormBuilder , private _apiServiceService: ApiServiceService) { }
 
   ngOnInit(): void {
+    this.searchForm = this.formBuilder.group({
+      search: '',
+    });
+    this.onChanges();
+  }
+  addToFav(data: any){
+    localStorage.setItem(data.id,JSON.stringify(data));
   }
 
+  onSearch(event:any){
+   console.log(event);
+  }
+  onChanges(): void {
+    this.searchForm.valueChanges.subscribe((val:any) => {
+      this.isLoading = true;
+      this._apiServiceService.getPokemon(val.search).subscribe((data) =>{
+        this.isLoading = false ;
+        this.data = data;
+        console.log(data);
+        this.error = undefined;
+        
+      } , (err) =>{
+        this.isLoading=false;
+        this.error = err.message;
+        this.data = undefined;
+      })
+    });
+  }
+
+  get f(){
+    return this.searchForm?.controls;
+  }
+  onSubmit(){
+    console.log(this.f['search'].value);
+  }
 }
