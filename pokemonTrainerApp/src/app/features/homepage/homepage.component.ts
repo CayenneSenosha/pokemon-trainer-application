29321a36c460abff11cbe2a17874b2ab62b826a1
Cain Senosha <csenosha@jhb.dvt.co.za>
2021-10-19 00:40:56 +0200

    PTA-20 adding navigations to the navbar & Alter CSS

--- pokemonTrainerApp/src/app/features/homepage/homepage.component.ts
+++ pokemonTrainerApp/src/app/features/homepage/homepage.component.ts
@@ -10,33 +10,34 @@ import { ApiServiceService } from 'src/app/service/api-service.service';
 })
 export class HomepageComponent implements OnInit {
 
-  searchForm: any ;
+  searchForm: any;
   isLoading: boolean = false;
   error: any = "";
-  pokemons: any  = [] ;
-  pokemonTemp: any  = [] ;
-  selectedPokemon: any ;
+  pokemons: any = [];
+  pokemonTemp: any = [];
+  selectedPokemon: any;
   noDataFound: boolean = true;
 
   favErrMsg: string = '';
-  
-  constructor(private formBuilder: FormBuilder , private _apiServiceService: ApiServiceService) { }
+
+  constructor(private formBuilder: FormBuilder, private _apiServiceService: ApiServiceService) { }
 
   ngOnInit(): void {
     this.searchForm = this.formBuilder.group({
       search: '',
     });
-    this.isLoading = true;
-    this._apiServiceService.getLimitPokemon().subscribe((data:any) =>{
-      this.isLoading = false ;
+
+      this.isLoading = true;
+      this._apiServiceService.getLimitPokemon().subscribe((data: any) => {
+      this.isLoading = false;
       this.noDataFound = false;
       console.log(data);
       this.pokemons = data.results;
       this.selectPokemon(this.pokemons[0].name);
       this.pokemonTemp = this.pokemons;
       this.error = undefined;
-    } , (err) =>{
-      this.isLoading=false;
+    }, (err) => {
+      this.isLoading = false;
       this.error = err.message;
       this.pokemons = undefined;
       this.pokemonTemp = this.pokemons;
@@ -45,59 +46,63 @@ export class HomepageComponent implements OnInit {
 
     this.onChanges();
   }
-  addToFav(data: any){
+
+  addToFav(data: any) {
     let fav: Favourites = {
-      key: data.id , value : data
+      key: data.id, value: data
     }
-    let favAdded =localStorage.getItem("favourites");
+
+    let favAdded = localStorage.getItem("favourites");
     let favourites: Favourites[] = [];
     this.favErrMsg = '';
-    
-    if(favAdded){
+
+    if (favAdded) {
       favourites = JSON.parse(favAdded);
-      if(favourites.filter(x => x.key == data.id).length > 0){
+      if (favourites.filter(x => x.key == data.id).length > 0) {
         console.log(`${data.name} already added as fav`);
-        this.favErrMsg = `${data.name} has already benn added as favourite pokeman`;
-      }else{
+        this.favErrMsg = `${data.name} has already been added as favourite pokeman`;
+      } else {
         favourites.push(fav);
-        localStorage.setItem("favourites",JSON.stringify(favourites));
+        localStorage.setItem("favourites", JSON.stringify(favourites));
       }
-    }else{
+    } else {
       favourites.push(fav);
-      localStorage.setItem("favourites",JSON.stringify(favourites));
+      localStorage.setItem("favourites", JSON.stringify(favourites));
     }
-      
+
   }
 
-  onSearch(event:any){
-   console.log(event);
+  onSearch(event: any) {
+    console.log(event);
   }
+
   onChanges(): void {
-    this.searchForm.valueChanges.subscribe((val:any) => {
-        this.pokemons = this.pokemonTemp.filter((x:any)=> x.name.includes(val.search));
-        if(this.pokemons.length == 0){
-            this.noDataFound = true;
-        }else{
-          this.noDataFound = false;
-        }
+    this.searchForm.valueChanges.subscribe((val: any) => {
+      this.pokemons = this.pokemonTemp.filter((x: any) => x.name.includes(val.search));
+      if (this.pokemons.length == 0) {
+        this.noDataFound = true;
+      } else {
+        this.noDataFound = false;
+      }
     });
   }
 
-  get f(){
+  get f() {
     return this.searchForm?.controls;
   }
-  onSubmit(){
+
+  onSubmit() {
     console.log(this.f['search'].value);
   }
 
-  pokemonClick(pokemon: any){
-      console.log(pokemon);
-      this.isLoading = true;
-      this.selectPokemon(pokemon.name);
+  pokemonClick(pokemon: any) {
+    console.log(pokemon);
+    this.isLoading = true;
+    this.selectPokemon(pokemon.name);
   }
 
-  selectPokemon(name: string){
-    this._apiServiceService.getPokemon(name).subscribe((p:any) =>{
+  selectPokemon(name: string) {
+    this._apiServiceService.getPokemon(name).subscribe((p: any) => {
       this.selectedPokemon = p;
       this.isLoading = false;
     })
